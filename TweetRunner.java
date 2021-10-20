b89da0612b6f53fe6cc0a0415be36aad9573055d
John <john497dit@hotmail.com>
2021-10-20 13:50:58 -0500

    update

--- TweetRunner.java
+++ TweetRunner.java
@@ -12,8 +12,20 @@ public class TweetRunner
 		Tweet sample = new Tweet( "aplus", 0, 0 );
 		sample.addLikes( 3 );
 		sample.addRetweets( 28 );
-		System.out.println( sample.notLiked() );  
-			  
+		System.out.println( sample.notLiked() );
+		System.out.println( sample.kindaLiked() );
+		sample.addLikes( 35 );
+		System.out.println( sample.kindaLiked() );
+		System.out.println( sample.isTrending() );
+		sample.addLikes( 13 );
+		sample.addRetweets( 47 );
+		System.out.println( sample.notLiked() );
+		System.out.println( sample.kindaLiked() );
+		System.out.println( sample.isTrending() );
+		System.out.println( sample );
+
+
+
 		//add more test code
 	}
 }
